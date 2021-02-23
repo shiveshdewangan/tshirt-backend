766b286eab217c444fa233912e88ad3510b137f6
Shivesh Dewangan <z1156965@wowcorp.com.au>
2021-02-23 20:05:14 +1100

    stripe changes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment");
 
 // middlewares
 app.use(cookieParser());
@@ -39,6 +40,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
 // Create Server
 const PORT = parseInt(process.env.PORT) || 80;
---
+++ controllers/stripepayment.js
@@ -0,0 +1,37 @@
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const uuidv1 = require("uuidv1");
+
+exports.makePayment = (req, res) => {
+  const { products, token } = req.body;
+
+  let amount = 0;
+  products.map((p) => {
+    amount = amount + p.price;
+  });
+
+  const idempotencyKey = uuidv1();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges
+        .create(
+          {
+            amount: amount,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            shipping: {
+              name: token.card.name,
+              address: token.card.address,
+            },
+          },
+          { idempotencyKey }
+        )
+        .then((result) => res.status(200).json(result))
+        .catch((err) => console.log(err));
+    });
+};
